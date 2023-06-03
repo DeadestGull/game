0a552d5b0aaf79b26c4da4dfcbf779aed843e275
Ryan Volkin <ryanvolkin@gmail.com>
2023-06-02 22:13:18 -0500

    added base for characters

--- index.js
+++ index.js
@@ -71,6 +71,18 @@ class Character
         this.damage=damage;
         this.attackSpeed=attackSpeed;
         this.hover=true;
+        this.size=size;
+    }
+    canPlace()
+    {
+        let bool=true;
+        characters.forEach(a=>
+        {
+            if (Math.sqrt(Math.pow(a.x-this.x,2)+Math.pow(a.y-this.y,2))<(this.size+a.size+25))
+                bool=false;
+        }    
+        );
+        return bool;
     }
     
 }
@@ -78,19 +90,24 @@ class Fire1 extends Character
 {
     constructor(x,y)
     {
-        let startingDamage=1;
-        startingAttackSpeed=2;
-        super(x,y,startingDamage,startingAttackSpeed,20);
+        super(x,y,1,2,35);
         this.cost=100;
     }
     paint()
     {
-        if (super.hover)
+
+        if (this.hover)
             ctx.globalAlpha =.4;
-        let img = new Image();
-        img.scr = 'images/imageTest.png';
-        ctx.drawImage(img,x,y);
+        /*let img = new Image();
+        img.src="images/imageTest.png";
+        ctx.drawImage(img,this.x,this.y);
+        */
+       ctx.beginPath();
+        ctx.fillStyle="red";
+        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+        ctx.fill();
         ctx.globalAlpha = 1;
+        ctx.fillStyle="black";
     }
 }
 let inMenu=true;
@@ -104,6 +121,8 @@ addEventListener("mousemove",onMouseMove);
 let levels=[
     new Level(500,500,150,400.0,1,1,1,5,1000,850)
 ];
+let characters=[];
+let characterHover=null;
 goToMap();
 function onMouseMove(event)
 {
@@ -126,12 +145,35 @@ function onClick(event)
     }
     else
     {
-        checkBuy();
+        if (characterHover!=null)
+        {
+            if (characterHover.canPlace())
+            {
+                characters.push(characterHover);
+                characterHover.hover=false;
+                money-=characterHover.cost;
+                characterHover=null;
+
+            }
+        }
+        checkBuy(event);
     }
 }
-function checkBuy()
+function checkBuy(e)
 {
-
+    if (buyMenu)
+    {
+        if (e.clientX-8>=1712&&e.clientX-8<=1762&&e.clientY-8>=375&&e.clientY-8<=525)//closeMenu
+            buyMenu=false;
+        
+        if (e.clientX-8>=1763&&e.clientX-8<=1812&&e.clientY-8>=400&&e.clientY-8<=500&&money>=100)//buy fire1
+            characterHover = new Fire1(mouseX,mouseY);
+    }
+    else
+    {
+        if (e.clientX-8>=1862&&e.clientX-8<=1912&&e.clientY-8>=375&&e.clientY-8<=525)//openMenu
+            buyMenu=true;
+    }
 }
 function goToMap()
 {
@@ -141,7 +183,7 @@ function startLevel(level)
 {
     inMenu=false;
     ctx.reset();
-    setInterval(function(){onTick(level)},50);
+    setInterval(function(){onTick(level)},20);
 }
 function onTick(level)
 {
@@ -150,6 +192,22 @@ function onTick(level)
         spawn(level);
     level.enemy.forEach(a=>moveEnemy(a,level));
     paintEnemy(level);
+    moveCharacters();
+    paintCharacters();
+}
+function moveCharacters()
+{
+    if (characterHover!=null)
+    {
+        characterHover.x=mouseX;
+        characterHover.y=mouseY;
+    }
+}
+function paintCharacters()
+{
+    characters.forEach(a=>a.paint());
+    if (characterHover!=null)
+        characterHover.paint();
 }
 function moveEnemy(enemy,level)
 {
@@ -170,29 +228,40 @@ function spawn(level)
     if (Math.random()<spawnOdds)
     {
         timeMult=(400-level.duration)/level.maxDuration*(level.maxTimeMult-1)+1;
-        console.log(level.duration,spawnOdds,level.timeLastSpawn);
         level.enemyBasic-=1;
         level.timeLastSpawn=0;
         if (Math.random()>=.5)
-            level.enemy.push(new EnemyBasic(Math.random()*1800.0,-50.0,1*level.damageMult*timeMult,2*level.speedMult,10*level.healthMult*timeMult));
+            level.enemy.push(new EnemyBasic(Math.random()*1800.0,-50.0,1*level.damageMult*timeMult,.75*level.speedMult,10*level.healthMult*timeMult));
         else if (Math.random()>=.5)
-            level.enemy.push(new EnemyBasic(-50.0,Math.random()*600.0,1*level.damageMult*timeMult,2*level.speedMult,10*level.healthMult*timeMult));  
+            level.enemy.push(new EnemyBasic(-50.0,Math.random()*600.0,1*level.damageMult*timeMult,.75*level.speedMult,10*level.healthMult*timeMult));  
         else
-            level.enemy.push(new EnemyBasic(2000.0,Math.random()*600.0,1*level.damageMult*timeMult,2*level.speedMult,10*level.healthMult*timeMult));
+            level.enemy.push(new EnemyBasic(2000.0,Math.random()*600.0,1*level.damageMult*timeMult,.75*level.speedMult,10*level.healthMult*timeMult));
     }
-    level.timeLastSpawn+=.25;
-    level.duration-=.05;
+    level.timeLastSpawn+=.08;
+    level.duration-=.02;
 }
 function paintBackgroundLevel()
 {
     ctx.reset();
     if (buyMenu)
     {
-        ctx.rect()
+        ctx.beginPath();
+        ctx.rect(1862,400,50,100);
+        ctx.rect(1812,400,50,100);
+        ctx.rect(1762,400,50,100);
+        ctx.rect(1862,500,50,100);
+        ctx.rect(1812,500,50,100);
+        ctx.rect(1762,500,50,100);
+        ctx.rect(1862,300,50,100);
+        ctx.rect(1812,300,50,100);
+        ctx.rect(1762,300,50,100);
+        ctx.stroke();
+        ctx.fillRect(1712,375,50,150);
     }
     else
     {
-        ctx.rect(1862,400,50,100);
+        ctx.beginPath();
+        ctx.rect(1862,375,50,150);
         ctx.fill();
     }
     //draw background here
